a67fa9e7d88710ed7520934b497ebc0af6c7f3b2
Hayato Yokoyama <hayato@MacBook-Pro.local>
2020-08-01 17:32:32 +0900

    [add]static_pagesコントローラーのGETリクエストが成功するかどうかのテストを追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Tabicolle"
+  end
+
+
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "ホーム | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
+    assert_select "title", "このアプリについて | #{@base_title}"
   end
-
 end

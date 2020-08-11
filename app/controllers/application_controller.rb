e9c182b96183ed087c13291ed37e92a2b3835296
Hayato Yokoyama <hayato@MacBook-Pro.local>
2020-08-12 06:52:26 +0900

    [add]登録フォームにusernameカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception #CSRF(サイトに攻撃用のコードを仕込み、アクセスしたユーザーに対して意図しない操作を行わせる攻撃)対策
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   protected
+    # 新規登録､更新をする際に登録するパラメーターを増やす
     def configure_permitted_parameters
-      devise_parameer_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username])
     end
 end

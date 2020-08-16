0226ad8e2f1753794d23fcb4d1495f172cde1e41
Hayato Yokoyama <hayato@MacBook-Pro.local>
2020-08-16 09:33:08 +0900

    [add]ユーザー詳細ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root 'static_pages#home'
   get '/about',  to:'static_pages#about'
+
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

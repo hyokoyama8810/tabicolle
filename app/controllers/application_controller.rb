dcaaf151d96fe0998bffe8e3cd993b0920d852cc
Hayato Yokoyama <hayato@MacBook-Pro.local>
2020-08-16 21:06:49 +0900

    [add]ユーザー詳細ページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception #CSRF(サイトに攻撃用のコードを仕込み、アクセスしたユーザーに対して意図しない操作を行わせる攻撃)対策
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後のリダイレクト先を指定
+  def after_sign_in_path_for(resource)
+    user_path(resource.id)
+  end
 
   protected
     # 新規登録､更新をする際に登録するパラメーターを増やす

3cb2c12a89afbeda32a894bf1ccb3065c9d88b7b
Hayato Yokoyama <hayato@MacBook-Pro.local>
2020-08-19 12:12:46 +0900

    [add]投稿一覧ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/about',  to:'static_pages#about'
 
   resources :users, :only => [:show]
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

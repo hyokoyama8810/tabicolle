47434d55721f1d23c6ef3506f13d569a9b098367
Hayato Yokoyama <hayato@MacBook-Pro.local>
2020-08-23 15:46:18 +0900

    [add]postsコントローラの認可テスト

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,17 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,10 +4,22 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   include Warden::Test::Helpers
 
   def setup
-    @user = users(:alice)
-    @other_user = users(:bob)
+    @post = posts(:Kanazawa)
   end
 
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Post.count' do
+      post posts_path, params: { post: { content: "金沢城に行ってきたよ!!" } }
+    end
+    assert_redirected_to new_user_session_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Post.count' do
+      delete post_path(@post)
+    end
+    assert_redirected_to new_user_session_url
+  end
 
 
 end

2402c106ee57f4419c52f6e79d58cea3f435abd1
h-yoko-web <yokohaya1027@gmail.com>
2020-09-10 10:52:41 +0900

    [add]フォロー､フォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   resources :users, :only => [:show]
   resources :posts, :only => [:index, :show, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
 end

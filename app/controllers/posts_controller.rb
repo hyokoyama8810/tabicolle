0d2f80757305e3669001e71874fc1c536b4191d4
h-yoko-web <yokohaya1027@gmail.com>
2020-09-13 09:42:20 +0900

    [add]モデルとコントローラーに追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:content, :image, :area, :genre, :season)
+      params.require(:post).permit(:content, :image, :area, :genre, :season, :tag_list)
     end
 
     def correct_user

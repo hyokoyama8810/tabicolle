41d4104fa4e3c61ad0e5c774a266181bbfbae098
h-yoko-web <yokohaya1027@gmail.com>
2020-09-26 13:36:50 +0900

    production.rbのaction_mailer設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,18 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => 'tabicolle.herokuapp.com' }
+
+  # mailer setting
+  config.action_mailer.default_url_options = { host: 'tabicolle.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.mailer_sender = 'yokohaya1027@gmail.com'
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'yokohaya1027@gmail.com',
+    :password => 'ehvwezxfewgvpuhe',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end

b6687c9c56df4d17d503b1f67d3c90149ababda4
h-yoko-web <yokohaya1027@gmail.com>
2020-10-01 10:42:11 +0900

    [fix]routes.rbの記載を変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users, :only => [:show]
   resources :posts
-  post '/posts/create', to:'posts#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す

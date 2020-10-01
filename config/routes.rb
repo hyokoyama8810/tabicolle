1f9841955f167883a00ec79358dfd1f6634c2a5d
h-yoko-web <yokohaya1027@gmail.com>
2020-10-01 10:16:27 +0900

    [update]アプリ紹介ページと利用規約ページを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
-  get '/about',  to:'static_pages#about'
-
-  post '/posts/create', to:'posts#create'
-
   resources :users, :only => [:show]
   resources :posts
+  post '/posts/create', to:'posts#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す

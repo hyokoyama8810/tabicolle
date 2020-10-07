3dba81aad03da20a6652b42376e9b5d3ffac9898
h-yoko-web <yokohaya1027@gmail.com>
2020-10-07 21:42:49 +0900

    [update]userとpostのアソシエーションのテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,8 @@
 FactoryBot.define do
-  factory :user, aliases: [:traveler]  do
+  factory :user do
     sequence(:name) { |n| "Test User#{n}"}
     sequence(:email) { |n| "tester#{n}@example.com"}
     sequence(:password) { |n| "testpw#{n}"}
     sequence(:password_confirmation) { |n| "testpw#{n}"}
-
-
-    # name {"Example User"}
-    # email {"user@example.com"}
-    # password {"foobar"}
-    # password_confirmation {"foobar"}
   end
 end

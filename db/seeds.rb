a48ee08a81b505b330cf3b16a01f98daff46407d
h-yoko-web <yokohaya1027@gmail.com>
2020-10-28 10:28:25 +0900

    [add]seedファイルで管理者ユーザーの初期情報を登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,12 @@
 #   content = Faker::Address.state + "の" + Faker::Address.city + "に行ってきた!!"
 #   Post.create!(content: content, user_id:1)
 # end
+
+User.create!(
+  email: 'admin@test.com',
+  name: '管理者',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: 'true',
+  confirmed_at: Time.now
+)

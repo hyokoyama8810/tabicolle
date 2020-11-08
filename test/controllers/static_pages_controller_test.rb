7eeaf34f5300f93cee3e698f7d4080a5feafd4c3
h-yoko-web <yokohaya1027@gmail.com>
2020-11-08 12:25:50 +0900

    [fix]テスト修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,9 +19,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "#{@base_title}"
   end
 
-  test "should get about" do
-    get about_path
-    assert_response :success
-    assert_select "title", "このアプリについて | #{@base_title}"
-  end
 end

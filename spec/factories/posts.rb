f26f3f633cc105e8af1d8cd71e5cce61e20a9b53
h-yoko-web <yokohaya1027@gmail.com>
2020-11-11 09:42:35 +0900

    [update]testpostのタグをランダム取得にした#22

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  areas = ['北海道･東北', '関東', '北陸･甲信越', '東海', '関西', '中国･四国', '九州･沖縄', '海外･その他']
+  genres = ['観光･散策', 'グルメ', '体験･アクティビティ', '温泉', 'スポーツ観戦', 'その他']
+  seasons = %w[春 夏 秋 冬]
+
   factory :post do
     sequence(:content) { |n| "testpost#{n}" }
-    area { '東海' }
-    genre { 'スポーツ観戦' }
-    season { '夏' }
+    area { areas.sample }
+    genre { genres.sample }
+    season { seasons.sample }
     association :user
   end
 end

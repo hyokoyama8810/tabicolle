5fa8a90630cd5277cfecebea79bdda4675e0a350
h-yoko-web <yokohaya1027@gmail.com>
2020-11-13 11:59:07 +0900

    [add]コントローラスペックの追加

--- spec/controllers /posts_controller_spec.rb
+++ spec/controllers /posts_controller_spec.rb
@@ -4,20 +4,34 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   describe '#index' do
-    before do
-      @user = create(:user)
+    context '認証済みのユーザーとして' do
+      before do
+        @user = create(:user)
+      end
+
+      it '正常にレスポンスを返すこと' do
+        sign_in @user
+        get :index
+        expect(response).to be_successful
+      end
+
+      it '200レスポンスを返すこと' do
+        sign_in @user
+        get :index
+        expect(response).to have_http_status '200'
+      end
     end
 
-    it '正常にレスポンスを返すこと' do
-      sign_in @user
-      get :index
-      expect(response).to be_successful
-    end
+    context '認証していないユーザーとして' do
+      it '302レスポンスを返すこと' do
+        get :index
+        expect(response).to have_http_status '302'
+      end
 
-    # it '200レスポンスを返すこと' do
-    #   sign_in @user
-    #   get :index
-    #   expect(response).to have_http_status '200'
-    # end
+      it 'サインイン画面にリダイレクトされること' do
+        get :index
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
   end
 end

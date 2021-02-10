5ec3fbeed0fd9d016803072c12c585900e8fc6eb
h-yoko-web <yokohaya1027@gmail.com>
2021-02-10 17:06:45 +0900

    [add]CourseのCRADの基本機能の実装 #35

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class CoursesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: %i[destroy edit update]
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = current_user.courses.build(course_params)
+    if @course.save
+      flash[:notice] = 'モデルコースが作成されました｡'
+      redirect_to courses_url
+    else
+      render 'courses/new'
+    end
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update(course_params)
+      flash[:notice] = 'モデルコースが編集されました'
+      redirect_to courses_url
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    flash[:notice] = '投稿を削除しました'
+    redirect_to request.referer || root_url
+    # [todo] 投稿詳細ページからのdestroyリクエスト後は､前ページにリダイレクトするのではなく､一覧ページに移動するようにしたい｡
+  end
+
+  private
+
+    def course_params
+      params.require(:course).permit(:title, :date, :genaral_memo)
+    end
+
+    def correct_user
+      @course = current_user.courses.find_by(id: params[:id])
+      redirect_to courses_url, alert: '他ユーザーのモデルコースの編集･削除はできません' if @course.nil? && current_user.admin == false
+    end
+end
---
+++ spec/requests/courses_request_spec.rb
@@ -0,0 +1,139 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Courses', type: :request do
+  before do
+    @user = create(:user)
+    @course = create(:course, user_id: @user.id)
+  end
+
+  describe 'GET #index' do
+    context '認証済みのユーザーとして' do
+      it '200レスポンスを返すこと' do
+        sign_in @user
+        get courses_path
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context '認証していないユーザーとして' do
+      it '302レスポンスを返すこと' do
+        get courses_path
+        expect(response).to have_http_status '302'
+      end
+
+      it 'サインイン画面にリダイレクトされること' do
+        get courses_path
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    context '認証済みのユーザーとして' do
+      it '200レスポンスを返すこと' do
+        sign_in @user
+        get new_course_path
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context '認証していないユーザーとして' do
+      it '302レスポンスを返すこと' do
+        get new_course_path
+        expect(response).to have_http_status '302'
+      end
+
+      it 'サインイン画面にリダイレクトされること' do
+        get new_course_path
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
+
+  describe 'course #create' do
+    context '認証済みのユーザーとして' do
+      it '投稿が作成されること' do
+        course_params = attributes_for(:course)
+        sign_in @user
+        expect {
+          post courses_path, params: { course: course_params }
+        }.to change(@user.courses, :count).by(1)
+      end
+    end
+
+    context '認証していないユーザーとして' do
+      it '投稿が登録されないこと' do
+        course_params = attributes_for(:course)
+        expect {
+          post courses_path, params: { course: course_params }
+        }.to change(@user.courses, :count).by(0)
+      end
+
+      it 'サインイン画面にリダイレクトされること' do
+        course_params = attributes_for(:course)
+        post courses_path, params: { course: course_params }
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    context '認証済みのユーザーとして' do
+      it '200レスポンスを返すこと' do
+        sign_in @user
+        get edit_course_path @course
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context '認証していないユーザーとして' do
+      it '302レスポンスを返すこと' do
+        get edit_course_path @course
+        expect(response).to have_http_status '302'
+      end
+
+      it 'サインイン画面にリダイレクトされること' do
+        get edit_course_path @course
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    context '認証済みのユーザーとして' do
+      it 'リクエストが成功すること' do
+        sign_in @user
+        delete course_url @course
+        expect(response).to have_http_status '302'
+      end
+
+      it '投稿が削除されること' do
+        sign_in @user
+        expect {
+          delete course_url @course
+        }.to change(Course, :count).by(-1)
+      end
+
+      it 'ユーザー一覧にリダイレクトすること' do
+        sign_in @user
+        delete course_url @course
+        expect(response).to redirect_to(request.referer || root_url)
+      end
+    end
+
+    context '認証していないユーザーとして' do
+      it '投稿が削除されず､投稿数が変わらないこと' do
+        expect {
+          delete course_url @course
+        }.to change(Course, :count).by(0)
+      end
+
+      it 'サインイン画面にリダイレクトすること' do
+        delete course_url @course
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
+end
